34b8a13721c4d9e8a24f8b0fdd163843902082b1
AdanYoruguaCAP88 <airules3@gmail.com>
2026-09-09 09:14:15 -0300

    Add Sopa de Letras app entry point

---
+++ app/src/main/java/com/classicsneox/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.classicsneox
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import com.classicsneox.wordsearch.presentation.ui.WordSearchScreen
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Surface {
+                    WordSearchScreen()
+                }
+            }
+        }
+    }
+}
