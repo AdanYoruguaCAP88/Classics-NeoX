99b7ddcdda7286095e836ef1a9e4494fa98dc4d9
AdanYoruguaCAP88 <airules3@gmail.com>
2026-09-09 23:19:59 -0300

    fix: compileSdk 37 + remove kotlin-android + add Compose getValue imports

--- mahjong/src/main/kotlin/com/classicsneox/mahjong/NeoXMahjongModule.kt
+++ mahjong/src/main/kotlin/com/classicsneox/mahjong/NeoXMahjongModule.kt
@@ -1,6 +1,7 @@
 package com.classicsneox.mahjong
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
